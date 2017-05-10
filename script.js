87c61ff8d27f53c9174523f4fcf696bab06765ec
sdabrowski <sdabrow2@gmail.com>
2017-05-09 23:11:11 -0500

    Finished Questions 1-5

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ function currentDate() {
     alert("The date is: " + date);
 }
 
-
 function isLeapYear(date) {
     if(date % 400 === 0){
         alert(date + " is a leap year.");
@@ -14,3 +13,54 @@ function isLeapYear(date) {
            alert(date + " is not a leap year.");
        }
 }
+
+function isArrayIncremental(preArray) {
+    var trigger = true;
+    var array = preArray.split(" ");
+    
+    if(array.length != 5){
+       alert("You didn't enter 5 numbers!");
+    } else{
+        for(i=0; i < (array.length - 1); i++){
+            if(trigger === true){
+                if((array[i+1]-array[i]) != 1){
+                trigger = false;
+                }
+            }
+        }
+    alert("Is the array consecutive? " + trigger);
+    }
+}
+
+function isArrayMultiple(preArray) {
+    var trigger = false;
+    var array = preArray.split(" ");
+    
+
+    for(i=0; i < (array.length - 1); i++){
+        if(trigger === false){
+            if((array[i+1]-array[i]) != 1){
+            trigger = true;
+            }
+        }
+    }
+    alert("Does these numbers contain multiples of the same value? " + trigger);
+}
+
+function first(array,n){
+    
+    var newArray = [];
+    
+    if(n == undefined){
+       n=1;
+    } else if(n > array.length){
+        n=array.length;
+    }
+    
+    for(i=0; i < n; i++){
+        newArray[i] = array[i]
+    }
+    return newArray;
+}
+
+
